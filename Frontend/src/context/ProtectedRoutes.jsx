12b0aeb28cc289cdedc841e00d585cef96281455
Mehroz farooq <85057779+Mehroz101@users.noreply.github.com>
2025-01-09 14:38:24 +0500

    Update ProtectedRoutes.jsx

--- Frontend/src/context/ProtectedRoutes.jsx
+++ Frontend/src/context/ProtectedRoutes.jsx
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 const ProtectedRoute = ({ element }) => {
   const navigate = useNavigate();
   const [isAuthenticated, setIsAuthenticated] = useState(true);
-
   useEffect(() => {
     const token = localStorage.getItem("feedbackapptoken");
     if (token) {
